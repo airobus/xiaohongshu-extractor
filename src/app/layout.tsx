54e50eaaa0aa85507c719a3ec694fb7fc591e280
juzi <zzh@youxiake.com>
2025-03-08 18:02:04 +0800

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
+    <html lang="zh">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
